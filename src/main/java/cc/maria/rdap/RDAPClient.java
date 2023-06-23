e06df5b06674f8e385e2c94e2fdae32718a04bdf
Maria Merkel <maria@maria.cc>
2023-06-23 13:08:31 +0200

    Bootstrapping for domain name space

---
+++ src/main/java/cc/maria/rdap/RDAPClient.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2023 Maria Merkel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the license at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package cc.maria.rdap;
+
+import cc.maria.rdap.bootstrap.DomainBootstrapRegistry;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+
+public class RDAPClient {
+    private String serviceURL = null;
+
+    private final Client client;
+
+    public RDAPClient () {
+        this (ClientBuilder.newClient());
+    }
+
+    public RDAPClient (String serviceURL) {
+        this ();
+        setServiceURL(serviceURL);
+    }
+
+    public RDAPClient (Client client) {
+        this.client = client;
+    }
+
+    public RDAPClient (String serviceURL, Client client) {
+        this (client);
+        setServiceURL(serviceURL);
+    }
+
+    private void setServiceURL (String serviceURL) {
+        if (!serviceURL.endsWith("/")) serviceURL += "/";
+        this.serviceURL = serviceURL;
+    }
+
+    /**
+     * Get the service URL for a given object.
+     * If the service URL is not set, the service URL will be looked up from the bootstrap registry.
+     * NOTE: RDAP servers can further redirect the client to another service URL. This method returns the first responsible service URL.
+     *
+     * @param object The object to be looked up
+     * @return The service URL responsible for the object
+     */
+    public String getServiceURL (String object) {
+        if (serviceURL != null) return serviceURL;
+        return DomainBootstrapRegistry.getInstance().getServiceURLForFQDN(object);
+    }
+}
---
+++ src/main/java/cc/maria/rdap/bootstrap/DomainBootstrapRegistry.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2023 Maria Merkel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the license at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package cc.maria.rdap.bootstrap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+
+/**
+ * RFC 9224-compliant wrapper around the IANA RDAP Bootstrap Service Registry for the Domain Name Space
+ */
+public class DomainBootstrapRegistry {
+    private HashMap<String, String> labelToServiceMap = new HashMap<>();
+
+    private static DomainBootstrapRegistry instance;
+
+    /**
+     * Get current instance of the wrapper
+     *
+     * @return DomainBootstrapRegistry instance
+     */
+    public static DomainBootstrapRegistry getInstance() {
+        if (instance == null) refresh();
+
+        return instance;
+    }
+
+    /**
+     * Throw away the current instance and get a new one with updated bootstrap data
+     *
+     * @return DomainBootstrapRegistry instance
+     */
+    public static DomainBootstrapRegistry refresh() {
+        try {
+            instance = new DomainBootstrapRegistry();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return instance;
+    }
+
+    private DomainBootstrapRegistry () throws IOException {
+        this (new URL("https://data.iana.org/rdap/dns.json"));
+    }
+
+    private DomainBootstrapRegistry (URL url) throws IOException {
+        JSONObject json = new JSONObject(new String(url.openStream().readAllBytes()));
+        JSONArray services = json.getJSONArray("services");
+
+        for (Object a : services) {
+            JSONArray array = (JSONArray) a;
+
+            for (Object s : array.getJSONArray(0)) {
+                labelToServiceMap.put((String) s, ((JSONArray) array.get(1)).getString(0));
+            }
+        }
+    }
+
+    /**
+     * Get the RDAP service URL for a given label
+     *
+     * @param label label to look up
+     * @return RDAP service URL
+     */
+    public String getServiceURL (String label) {
+        return labelToServiceMap.get(label);
+    }
+
+    /**
+     * Get the RDAP service URL for a given FQDN
+     *
+     * @param fqdn FQDN to look up
+     * @return RDAP service URL
+     */
+    public String getServiceURLForFQDN (String fqdn) {
+        if (labelToServiceMap.containsKey(fqdn)) return labelToServiceMap.get(fqdn);
+        if (!fqdn.contains(".")) return null;
+        return getServiceURLForFQDN(fqdn.substring(fqdn.indexOf('.') + 1));
+    }
+}
