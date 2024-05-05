46f00062d54624b96fe7515ecbf8344b132a7114
Maria Merkel <maria@maria.cc>
2024-05-05 09:56:20 +0200

    Fixed Maven publishing preparation script

--- build-maven-central.sh
+++ build-maven-central.sh
@@ -1,6 +1,6 @@
-for file in target/*.jar; do md5sum "$file" > "$file".md5; done
-for file in target/*.jar; do sha1sum "$file" > "$file".sha1; done
+for file in target/*.jar; do md5sum "$file" | cut -c -32 > "$file".md5; done
+for file in target/*.jar; do sha1sum "$file" | cut -c -40 > "$file".sha1; done
 mkdir pom
 cp pom.xml pom/pom.xml
-md5sum pom/pom.xml > pom/pom.xml.md5
-sha1sum pom/pom.xml > pom/pom.xml.sha1
+md5sum pom/pom.xml | cut -c -32 > pom/pom.xml.md5
+sha1sum pom/pom.xml | cut -c -40 > pom/pom.xml.sha1
